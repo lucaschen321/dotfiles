a09ef18c0a8279622506b418b93b06072693fd4a
Lucas Chen <lucaschen321@gmail.com>
2017-06-23 16:54:15 -0700

    Fix referencing typo

--- setup.sh
+++ setup.sh
@@ -109,10 +109,10 @@ install_zsh() {
         # - zsh-syntax-highlighting
 
          # Install zsh-autosuggestions
-         git clone git://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+         git clone git://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 
          # Install zsh-syntax-highlighting
-         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
         # Edit /etc/shells to incorporate installed zsh path if not present
         zsh=$(which zsh)
